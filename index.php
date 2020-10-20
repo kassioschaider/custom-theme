9754b7b86e30c6d2c1bd6f0168f4c563de4119b2
Kassio <kassio.schaider@hotmail.com>
2020-10-20 19:52:34 -0300

    Adicionando posts

--- index.php
+++ index.php
@@ -14,8 +14,33 @@
             <section class="middle-area">
                 <div class="container">
                     <div class="row">
-                        <aside class="sidebar">Barras Lateral</aside>
-                        <div class="news">Notícias</div>
+                        <aside class="sidebar col-md-4">Barras Lateral</aside>
+                        <div class="news col-md-8">
+
+                            <?php
+                            //Se houver algum post
+                            if( have_posts() ):
+                                //Enquanto houver posts, mostre-os
+                                while( have_posts() ): the_post();
+                            ?>
+
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                <p>Categories: <?php the_category( ' ' ); ?></p>
+                                <p><?php the_tags( 'Tags: ', ', ' )?></p>
+                                <?php echo get_the_content(); ?>
+                            </article>
+
+                            <?php
+                                endwhile;
+                            else:
+                            ?>
+                            <p>There's nothing yet to be displayed...</p>
+
+                            <?php endif; ?>
+
+                        </div>
                     </div>
                 </div>
             </section>
