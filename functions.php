cb7ebfd3d2f75b0453eefab6b2076d93782bc337
Kassio <kassio.schaider@hotmail.com>
2020-10-23 18:04:32 -0300

    Widget de serviços e atualizaçao do CSS

--- functions.php
+++ functions.php
@@ -33,9 +33,10 @@ function custom_theme_config()
 }
 add_action( 'after_setup_theme', 'custom_theme_config', 0 );
 
-//Adicionando sidebars
+//Adicionando widgets
 add_action( 'widgets_init', 'custom_theme_sidebars' );
 function custom_theme_sidebars() {
+    //Adcionando sidebars
     register_sidebar( 
         array(
             'name' => 'Home Page Sidebar',
@@ -52,6 +53,40 @@ function custom_theme_sidebars() {
             'name' => 'Blog Sidebar',
             'id' => 'sidebar-2',
             'description' => 'Sidebar to be used on Home Page for Blog',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after-title' => '</h2>'
+        )
+    );
+    //Adicionando services widget
+    register_sidebar( 
+        array(
+            'name' => 'Services 1',
+            'id' => 'services-1',
+            'description' => 'First Services Area.',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after-title' => '</h2>'
+        )
+    );
+    register_sidebar( 
+        array(
+            'name' => 'Services 2',
+            'id' => 'services-2',
+            'description' => 'Second Services Area.',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after-title' => '</h2>'
+        )
+    );
+    register_sidebar( 
+        array(
+            'name' => 'Services 3',
+            'id' => 'services-3',
+            'description' => 'Third Services Area.',
             'before_widget' => '<div class="widget-wrapper">',
             'after_widget' => '</div>',
             'before_title' => '<h2 class="widget-title">',
